5a4a28d30273164b861a469c6cc25ddb4c79ad70
GUSTAVO GORGES KOCH <gustavo_koch@estudante.sesisenai.org.br>
2023-04-27 21:58:01 -0300

    implementa formulario

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,58 @@
 import { Component } from '@angular/core';
 
+interface Tarefa {
+  nomeTarefa:string;
+  classeTarefa:string;
+}
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'todo-app';
+
+  tarefas : Tarefa[] = [];
+  
+  mostraInput: boolean = true;
+  mostraTarefa: boolean = true;
+  displayTarefa: string = "";
+
+  tarefa:Tarefa = {
+    nomeTarefa: "",
+    classeTarefa: ""
+  }
+
+  esconderInput(): void{
+    this.mostraInput = false;
+  }
+
+  mostrarInput(): void{
+    this.mostraInput = true;
+  }
+
+  mostrarTarefa(): void {
+    this.mostraTarefa= false;
+  }
+
+  cadastrarTarefa(): void{
+
+    const tarefaList:Tarefa ={
+      nomeTarefa: this.tarefa.nomeTarefa,
+      classeTarefa: this.tarefa.classeTarefa
+    }
+
+    this.tarefas.push(tarefaList)
+    localStorage.setItem("Tarefas", JSON.stringify(this.tarefas))
+    console.log(this.tarefas)
+    
+    this.tarefa.nomeTarefa = "";
+    
+  
+  }
+
+  removerTarefa(indice:number):void {
+    this.tarefas.splice(indice, 1)
+  }
+
 }
