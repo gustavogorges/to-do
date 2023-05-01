a3e8a5d3957b04cbfd59f32c9a1203b44002ffb0
Gustavo Gorges Koch <gustavo_gorges@hotmail.com>
2023-05-01 16:03:22 -0300

    implementa local storage funcional

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,58 +1,79 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 interface Tarefa {
-  nomeTarefa:string;
-  classeTarefa:string;
+  nomeTarefa: string;
+  classeTarefa: string;
+  codigoTarefa: number;
 }
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  session: any;
+  tarefas: Tarefa[] = [];
 
-  tarefas : Tarefa[] = [];
-  
   mostraInput: boolean = true;
   mostraTarefa: boolean = true;
   displayTarefa: string = "";
 
-  tarefa:Tarefa = {
-    nomeTarefa: "",
-    classeTarefa: ""
+  ngOnInit() {
+    if (localStorage.getItem("Tarefas") != null) {
+      this.tarefas = JSON.parse(localStorage.getItem("Tarefas"));
+    }
   }
 
-  esconderInput(): void{
+  tarefa: Tarefa = {
+    nomeTarefa: "",
+    classeTarefa: "",
+    codigoTarefa: 0,
+  };
+
+  esconderInput(): void {
     this.mostraInput = false;
   }
 
-  mostrarInput(): void{
+  mostrarInput(): void {
     this.mostraInput = true;
   }
 
   mostrarTarefa(): void {
-    this.mostraTarefa= false;
+    this.mostraTarefa = false;
   }
 
-  cadastrarTarefa(): void{
-
-    const tarefaList:Tarefa ={
+  cadastrarTarefa(): void {
+    const tarefaList: Tarefa = {
       nomeTarefa: this.tarefa.nomeTarefa,
-      classeTarefa: this.tarefa.classeTarefa
-    }
+      classeTarefa: this.tarefa.classeTarefa,
+      codigoTarefa: this.tarefa.codigoTarefa++,
+    };
 
-    this.tarefas.push(tarefaList)
-    localStorage.setItem("Tarefas", JSON.stringify(this.tarefas))
-    console.log(this.tarefas)
-    
+    console.log(tarefaList);
+
+    this.tarefas.push(tarefaList);
+
+    localStorage.setItem("Tarefas", JSON.stringify(this.tarefas));
+
+    // let tarefasInit: any = localStorage.getItem("Tarefas");
+    // this.session = JSON.parse(tarefasInit);
+    // console.log(this.session);
+
+    // localStorage.setItem(
+    //   "TarefaClasse",
+    //   JSON.stringify(this.tarefa.classeTarefa)
+    // );
+    // localStorage.setItem(
+    //   "TarefaCodigo",
+    //   JSON.stringify(this.tarefa.codigoTarefa)
+    // );
+    console.log(this.tarefas);
+
     this.tarefa.nomeTarefa = "";
-    
-  
   }
 
-  removerTarefa(indice:number):void {
-    this.tarefas.splice(indice, 1)
+  removerTarefa(indice: number): void {
+    this.tarefas.splice(indice, 1);
   }
-
 }
