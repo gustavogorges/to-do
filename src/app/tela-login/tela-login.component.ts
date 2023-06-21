747f0192833fb9dff24337ad791c8570c06091bf
gustavogorges <gustavo_gorges@hotmail.com>
2023-06-20 22:30:47 -0300

    importa a classe user do models na tela de login porém não funcional

--- src/app/tela-login/tela-login.component.ts
+++ src/app/tela-login/tela-login.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from 'src/models/users/user';
+import { UserRepository } from 'src/repositories/user.repository'
+
+
 
 @Component({
   selector: 'app-tela-login',
@@ -7,7 +11,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TelaLoginComponent implements OnInit {
 
-  constructor() { }
+  private userId : string = "joao.silva"
+  private users: User[] = [];
+  public user : User | undefined;
+
+  constructor(
+    private UserRepository: UserRepository
+  ) { 
+    this.users = this.UserRepository.getUsers();
+    this.user = this.getUsuarioLogado();
+    console.log(this.user)
+  }
+
+  private getUsuarioLogado(): User | undefined {
+    return this.users.find((user) => {
+      return user.id === this.userId;
+    });
+  }
+
+  verificaLogin():void {
+    console.log('test')
+  }
 
   ngOnInit() {
   }
