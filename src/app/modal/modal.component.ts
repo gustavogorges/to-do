6cf370fbceb7c16278a599ffce195fdff5119b5f
gustavogorges <gustavo_gorges@hotmail.com>
2023-06-22 18:40:04 -0300

    estilização do registro

--- src/app/modal/modal.component.ts
+++ src/app/modal/modal.component.ts
@@ -22,70 +22,9 @@ export class ModalComponent implements OnInit {
   constructor(
     private UserRepository: UserRepository
     ) {} 
-  
-
-
-  @Output() clicouFecharModal = new EventEmitter();
-
-  @Output() clicouCadastrarTipo = new EventEmitter();
-
-  ngOnInit() {
-    let cores:String[];
-    cores = JSON.parse(localStorage.getItem("CoresGeral"));
-    if( cores != null ){
-      this.coresGeral = cores;
-    }
-
-    if (JSON.parse(localStorage.getItem("CategoriasGeral")) != null) {
-      this.categoriasGeral = JSON.parse(
-        localStorage.getItem("CategoriasGeral")
-      );
-    }
-
-    this.userId = localStorage.getItem('UsuarioLogado');
-
-    this.users = this.UserRepository.getUsers();
-    this.user = this.getUsuarioLogado();
-
-    console.log(this.user)
-  }
-
-  private getUsuarioLogado(): User | undefined {
-    return this.users.find((user) => {
-      return user.id === this.userId;
-    });
-  }
-
-  removerClasse(indice: number): void {
-    console.log(indice);
-    this.categoriasGeral.splice(indice, 1);
-    this.coresGeral.splice(indice,1);
-    localStorage.setItem(
-      "CategoriasGeral",
-      JSON.stringify(this.categoriasGeral)
-    );
-    localStorage.setItem(
-      "CoresGeral",
-      JSON.stringify(this.coresGeral)
-    );
-  }
-
-  esconderModal(): void {
-    this.clicouFecharModal.emit();
-  }
-
-  cadastrarTipo(): void {
-    this.coresGeral.push(this.tarefaCor);
-    this.categoriasGeral.push(this.tarefaTipo);
-
-    localStorage.setItem("CoresGeral", JSON.stringify(this.coresGeral));
-    localStorage.setItem(
-      "CategoriasGeral",
-      JSON.stringify(this.categoriasGeral)
-    );
-    this.tarefaTipo = "";
-    this.tarefaCor = "";
-
+  ngOnInit(): void {
     
   }
+  
+
 }
--- src/app/propriedade/propriedade.component.ts
+++ src/app/propriedade/propriedade.component.ts
@@ -2,13 +2,6 @@ import { Component, EventEmitter, OnInit, Output } from '@angular/core';
 import { User } from 'src/models/users/user';
 import { UserRepository } from 'src/repositories/user.repository'
 
-interface Propriedade {
-  nomePropriedade: string;
-  tipoDado: string;
-  indicePropriedade: number;
-  valorPropriedade: String;
-}
-
 @Component({
   selector: 'app-propriedade',
   templateUrl: './propriedade.component.html',
@@ -25,94 +18,10 @@ export class PropriedadeComponent implements OnInit {
     ) {} 
   
 
-  propriedadesGeral: Propriedade[] = [];
-  categoriasGeral : String[] = [];
-  tipoDado: string;
-
-  propertieAddBoolean : boolean = true;
-  propertieEditBoolean : boolean = true;
-  propertieMoveBoolean : boolean = true;
-  propertieRemoveBoolean : boolean = true;
-
-
-  @Output() clicouFecharModal = new EventEmitter();
-
-  @Output() clicouCadastrarTipo = new EventEmitter();
-
   ngOnInit() {
-    this.categoriasGeral = JSON.parse(localStorage.getItem("CategoriasGeral"))
-
-    if (JSON.parse(localStorage.getItem("PropriedadesGeral")) != null) {
-      this.propriedadesGeral = JSON.parse(
-        localStorage.getItem("PropriedadesGeral")
-      );
-    }
-
-    this.userId = localStorage.getItem('UsuarioLogado');
-
-    this.users = this.UserRepository.getUsers();
-    this.user = this.getUsuarioLogado();
-
-    this.disabled();
-
-    console.log(this.user)
-  }
-
-  disabled() : void {
-    console.log('entrou')
-    this.user.propertiesPermissions.forEach(permission => {
-      if(permission == 'Add') {
-        console.log('entrou')
-        this.propertieAddBoolean = false;
-      }
-    });
-  }
-
-  removerClasse(indice: number): void {
-    console.log(indice);
-    this.propriedadesGeral.splice(indice, 1);
-    localStorage.setItem(
-      "PropriedadesGeral",
-      JSON.stringify(this.propriedadesGeral)
-    );
-  }
-
-  esconderModal(): void {
-    this.clicouFecharModal.emit();
-  }
-
-  propriedade: Propriedade = {
-    nomePropriedade: "",
-    tipoDado: "",
-    indicePropriedade: 0,
-    valorPropriedade: ""
-  };
-
-  private getUsuarioLogado(): User | undefined {
-    return this.users.find((user) => {
-      return user.id === this.userId;
-    });
-  }
-
-  cadastrarTipo(): void {
     
-    const propriedadeNova : Propriedade  = {
-      nomePropriedade : this.propriedade.nomePropriedade,
-      tipoDado : this.propriedade.tipoDado,
-      indicePropriedade : this.propriedadesGeral.length,
-      valorPropriedade : this.propriedade.valorPropriedade
-    }
-    
-console.log(propriedadeNova.indicePropriedade)
-
-    this.propriedadesGeral.push(propriedadeNova)
-
-    localStorage.setItem(
-      "PropriedadesGeral",
-      JSON.stringify(this.propriedadesGeral)
-    )
-
- 
   }
+
+  
 
 }
